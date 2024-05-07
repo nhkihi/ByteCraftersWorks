c2a6f16894eaa805a5bc60ba8af214b37826fc3e
nhkihi <psjceterwohlers@hotmail.com>
2024-05-07 14:47:28 +0700

    test commit

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-function combinationSum3(k, n) {
-  const result = [];
-  backtrack([], 1, k, n);
-  return result;
-  function backtrack(combination, start, k, n) {
-    if (n === 0 && k === 0) {
-      result.push([...combination]);
-      return;
-    }
-    if (n < 0 || k === 0) return;
-    for (let i = start; i <= 9; i++) {
-      combination.push(i);
-      backtrack(combination, i + 1, k - 1, n - i);
-      combination.pop();
-    }
-  }
-}
+const name = "john";
+const upperName = name.toUpperCase();
+console.log(upperName); // JOHN
