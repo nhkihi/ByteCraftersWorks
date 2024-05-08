4f21e1f98098a2403827061277061349404d90ec
nhkihi <psjceterwohlers@hotmail.com>
2024-05-08 21:28:58 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const name = "john";
-const upperName = name.toUpperCase();
-console.log(upperName); // JOHN
+function rob(nums) {
+  if (nums.length === 1) return nums[0];
+  const robRange = (start, end) => {
+    let prevMax = 0;
+    let currMax = 0;
+    for (let i = start; i <= end; i++) {
+      const temp = currMax;
+      currMax = Math.max(currMax, prevMax + nums[i]);
+      prevMax = temp;
+    }
+    return currMax;
+  };
+  return Math.max(robRange(0, nums.length - 2), robRange(1, nums.length - 1));
+}
