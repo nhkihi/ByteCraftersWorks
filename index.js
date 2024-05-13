25778c9dc0de74cbf7547c1d5161428d375370ca
nhkihi <psjceterwohlers@hotmail.com>
2024-05-13 22:39:20 +0700

    test project

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function canPartition(nums) {
-  const sum = nums.reduce((acc, val) => acc + val, 0);
-  if (sum % 2 !== 0) return false;
-  const target = sum / 2;
-  const dp = new Array(target + 1).fill(false);
-  dp[0] = true;
-  for (const num of nums) {
-    for (let i = target; i >= num; i--) {
-      dp[i] = dp[i] || dp[i - num];
-    }
-  }
-  return dp[target];
+function sumArray(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
 }
