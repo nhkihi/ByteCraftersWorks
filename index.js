12612ab3d187764f9721b7feb6518e546f5bbba2
nhkihi <psjceterwohlers@hotmail.com>
2024-05-15 10:37:27 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function countConsonants(str) {
-  return str.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
+function isValidParentheses(s) {
+  const stack = [];
+  const map = { "(": ")", "[": "]", "{": "}" };
+  for (const char of s) {
+    if (char in map) stack.push(char);
+    else {
+      const top = stack.pop();
+      if (map[top] !== char) return false;
+    }
+  }
+  return stack.length === 0;
 }
